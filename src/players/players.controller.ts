1dd1fa3feef1fa195275b6c51c1191702abfe1df
Cleiton <cleitonwoycik@outlook.com>
2021-07-06 15:18:38 -0300

    creating player controller

---
+++ src/players/players.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/create-player.dto';
+
+@Controller('api/v1/players')
+export class PlayersController {
+
+    @Post()
+    async createAndUpdatePlayer(
+        @Body() createPlayerDto: CreatePlayerDto
+    ) {
+        const { email } = createPlayerDto;
+        return JSON.stringify({
+            email: email,
+        });
+    }
+}
