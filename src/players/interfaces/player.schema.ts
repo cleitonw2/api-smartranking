dfdecb068fb73db00ab6f96c6d221c2b3fd3aff5
Cleiton <cleitonwoycik@outlook.com>
2021-07-08 21:44:38 -0300

    adding mongodb

---
+++ src/players/interfaces/player.schema.ts
@@ -0,0 +1,11 @@
+import * as mongoose from "mongoose";
+
+
+export const PlayerSchema = new mongoose.Schema({
+    phone: { type: String, unique: true },
+    email: { type: String, unique: true },
+    name: String,
+    ranking: String,
+    rankingPosition: Number,
+    photoUrl: String,
+}, { timestamps: true, collection: "players" });
